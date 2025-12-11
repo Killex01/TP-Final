eda19a775f87d063ff0164ebaab3c044302fde44
Killex01 <LOVINGJAEMIN@GMAIL.COM>
2025-12-11 01:13:43 -0500

    - ajout deux animation (transition et la rotation des images astres) - rajout de code JS pour mettre le bouton fonctionnel - ajout de commentaires - ajout de la page ressource avec un lien - ajout de deux images

--- TPFinal/src/main.js
+++ TPFinal/src/main.js
@@ -1,2 +1,14 @@
 import './styles/style.css'
 
+
+ const myButton = document.getElementById("bouton");
+ let game = document.getElementById("experience-test");
+ let mainMenu = document.getElementById("hero-wrapper");
+ 
+ myButton.addEventListener("click", showGame); 
+
+
+  function showGame() {
+    game.style.display = "block";
+    mainMenu.style.display = "none";
+  }
