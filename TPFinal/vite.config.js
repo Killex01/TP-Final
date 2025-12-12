3c473797b0b9b2bba2a0a05d65cdc83be1db2d4b
Killex01 <LOVINGJAEMIN@GMAIL.COM>
2025-12-11 22:00:05 -0500

    yes

--- TPFinal/vite.config.js
+++ TPFinal/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/tpFinal_Alex_Deandrade-Lim_Masato_Charbonneau/",
+    base: "/tpFinal_Alex_Deandrade_Lim_Masato_Charbonneau/",
     build: {
         emptyOutDir: true,
         rollupOptions:{
